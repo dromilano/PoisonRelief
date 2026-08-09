e2962be15d79e2f3da19c662d2bba2d3b6826b1a
Dro <dromilano@icloud.com>
2026-08-09 19:23:25 -0300

    Fix tablet use and Build 42 localization

---
+++ Contents/mods/PoisonRelief/common/media/lua/shared/PoisonRelief/PoisonRelief_TakeTabletAction.lua
@@ -0,0 +1,93 @@
+require "PoisonRelief/PoisonRelief_Treatment"
+require "TimedActions/ISBaseTimedAction"
+
+PoisonReliefTakeTabletAction = ISBaseTimedAction:derive(
+    "PoisonReliefTakeTabletAction"
+)
+
+local function findTabletById(character, itemId)
+    if not character or not itemId then return nil end
+
+    local inventory = character:getInventory()
+    return inventory:getItemById(itemId)
+        or inventory:getItemWithIDRecursiv(itemId)
+end
+
+local function isUsableTablet(item)
+    if not item or not PoisonRelief.TREATMENTS[item:getFullType()] then
+        return false
+    end
+
+    return not item:IsDrainable() or item:getUsedDelta() > 0
+end
+
+function PoisonReliefTakeTabletAction:isValid()
+    if self.didComplete or not self.character then return false end
+
+    if isClient() then
+        return isUsableTablet(findTabletById(
+            self.character,
+            self.itemId
+        ))
+    end
+
+    return self.item
+        and self.character:getInventory():containsRecursive(self.item)
+        and isUsableTablet(self.item)
+end
+
+function PoisonReliefTakeTabletAction:update()
+    self.item:setJobDelta(self:getJobDelta())
+    self:setActionAnim(CharacterActionAnims.TakePills)
+end
+
+function PoisonReliefTakeTabletAction:start()
+    if isClient() then
+        self.item = findTabletById(self.character, self.itemId)
+    end
+
+    self.item:setJobType(getText(
+        "ContextMenu_PoisonRelief_TakeTablet"
+    ))
+    self.item:setJobDelta(0.0)
+    self:setOverrideHandModels(nil, self.item)
+end
+
+function PoisonReliefTakeTabletAction:stop()
+    if self.item then self.item:setJobDelta(0.0) end
+    ISBaseTimedAction.stop(self)
+end
+
+function PoisonReliefTakeTabletAction:perform()
+    if self.item then self.item:setJobDelta(0.0) end
+    ISBaseTimedAction.perform(self)
+end
+
+function PoisonReliefTakeTabletAction:complete()
+    if self.didComplete or not self:isValid() then return false end
+
+    if isClient() then
+        self.item = findTabletById(self.character, self.itemId)
+    end
+
+    self.didComplete = true
+    if self.character:isLocalPlayer()
+        and PoisonRelief.completeTabletAction then
+        PoisonRelief.completeTabletAction(self.item, self.playerNum)
+    end
+    return true
+end
+
+function PoisonReliefTakeTabletAction:new(character, item, playerNum)
+    local action = ISBaseTimedAction.new(self, character)
+    action.item = item
+    action.itemId = item:getID()
+    action.playerNum = playerNum
+    action.didComplete = false
+    action.stopOnWalk = true
+    action.stopOnRun = true
+    action.stopOnAim = true
+    action.maxTime = character:isTimedActionInstant() and 1 or 80
+    action.isEating = true
+    return action
+end
